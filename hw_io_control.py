64c31b73e448b0d6af04329e7cc61699203c8fb1
dugbang <dugbang@gmail.com>
2018-08-08 23:25:39 +0900

    * 프로세스별 파일 분리 * 메인 프로세스 클래스화

---
+++ hw_io_control.py
@@ -0,0 +1,23 @@
+import os
+import queue
+import time
+
+
+def hw_io_control_process(q, q_main):
+    print('hw_io_control_process pid; {}'.format(os.getpid()))
+    i = 0
+    while True:
+        try:
+            msg = q.get(False)
+        except queue.Empty:
+            time.sleep(1)
+            i += 1
+            print('hw io sleep {}'.format(i))
+            if i > 5:
+                print('time out...')
+                break
+        else:
+            print(msg)
+            if msg[0] == 'quit':
+                q_main.put(('quit', 'main...'))
+                break
--- process_test.py
+++ process_test.py
@@ -3,70 +3,94 @@ import time
 from multiprocessing import Queue, Process
 import queue
 
-
-def usb_capture_process(q, q_main):
-    i = 0
-    while True:
-        try:
-            msg = q.get(False)
-        except queue.Empty:
-            time.sleep(1)
-            i += 1
-            print('usb sleep {}'.format(i))
-            if i > 5:
-                print('time out...')
-                break
-        else:
-            print(msg)
-            if msg[0] == 'quit':
-                q_main.put(('quit', 'test'))
-                break
-            # Handle task here and call q.task_done()
+from hw_io_control import hw_io_control_process
+from usb_capture import usb_capture_process
 
 
-def hw_io_control_process():
-    pass
+class Communication:
+    """
+    메인프로세스로 main 에서 실행된다.
+    """
+    def __init__(self):
+        self.__queue = Queue()
+        self.__q_usb = Queue()
+        self.__q_hwio = Queue()
 
+        self.__process = (
+            Process(target=usb_capture_process, args=(self.__q_usb, self.__queue)),
+            Process(target=hw_io_control_process, args=(self.__q_hwio, self.__queue)),
+        )
 
-def f(q):
-    q.put([42, None, 'hello'])
+    def __run(self):
+        while True:
+            try:
+                msg = self.__queue.get(False)
+            except queue.Empty:
+                print('main sleep 1')
+                time.sleep(1)
+                self.__q_usb.put(('quit', 'uab test...'))
+                self.__q_hwio.put(('quit', 'hw io test...'))
+            else:
+                print(msg)
+                if msg[0] == 'quit':
+                    break
+
+    def start(self):
+        print('main process pid; {}'.format(os.getpid()))
+
+        for process in self.__process:
+            process.start()
+
+        time.sleep(2)
+        self.__run()
+
+        for process in self.__process:
+            process.join()
+
+    def __del__(self):
+        print('Communication del... ')
+        self.__queue.close()
+        self.__q_usb.close()
+        self.__q_hwio.close()
 
 
 def test_process():
 
-    # q = Queue()
-    # p = Process(target=f, args=(q,))
-    # p.start()
-    # print(q.get())    # prints "[42, None, 'hello']"
-    # p.join()
+    com = Communication()
+    com.start()
 
-    q_usb = Queue()
+    # q_usb = Queue()
     # q_hwio = Queue()
-    q_main = Queue()
-
-    print(os.getpid())
-
-    process_one = Process(target=usb_capture_process, args=(q_usb, q_main))
-    process_one.start()
-    time.sleep(2)
-    # q_usb.put(('quit', 'test'))
+    # q_main = Queue()
     #
+    # print('main process pid; {}'.format(os.getpid()))
+    #
+    # process_usb = Process(target=usb_capture_process, args=(q_usb, q_main))
+    # process_hwio = Process(target=hw_io_control_process, args=(q_hwio, q_main))
+    # process_usb.start()
+    # process_hwio.start()
+    # time.sleep(2)
+    # # q_usb.put(('quit', 'test'))
+    # #
     # while True:
     #     try:
     #         msg = q_main.get(False)
     #     except queue.Empty:
     #         print('main sleep 1')
     #         time.sleep(1)
+    #         q_usb.put(('quit', 'uab test...'))
+    #         q_hwio.put(('quit', 'hw io test...'))
     #     else:
     #         print(msg)
     #         if msg[0] == 'quit':
     #             break
-
-    process_one.join()
-
-    q_usb.close()
+    #
+    # process_usb.join()
+    # process_hwio.join()
+    #
+    # q_usb.close()
     # q_hwio.close()
-    q_main.close()
+    # q_main.close()
 
 
 if __name__ == "__main__":
